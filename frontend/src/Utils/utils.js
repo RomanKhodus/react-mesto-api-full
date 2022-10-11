52701346f44eaaeb148cd3ec335fb7d5440167a6
Roman Khodus <rkhodus@gmail.com>
2022-10-11 09:55:23 +0700

    build PW15

--- frontend/src/Utils/utils.js
+++
@@ -1,8 +0,0 @@
- export default function renderLoading(buttonSelector, isLoading) {
-    const buttonSubmit = document.querySelector(buttonSelector);
-    if (isLoading) {
-      buttonSubmit.textContent = "Сохранение...";
-    } else {
-      buttonSubmit.textContent = "Сохранить";
-    }
-  }
