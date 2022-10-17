b6edcc39ffee0664ff0186b61efe2612e161581f
Roman Khodus <rkhodus@gmail.com>
2022-10-17 14:35:04 +0700

    apdate lofin from controllers/users

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -36,11 +36,11 @@ module.exports.login = (req, res, next) => {
   User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.cookie('jwt', token, {
-        maxAge: 3600 * 24 * 7,
-        sameSite: 'none',
-        secure: true,
-      });
+      // res.cookie('jwt', token, {
+      //   maxAge: 3600 * 24 * 7,
+      //   sameSite: 'none',
+      //   secure: true,
+      // });
       res.send({
         message: 'Авторизация прошла успешно',
         token,
